9c9770698051d9325dce120cae7d0e446d8e8820
Franck Janel AGAH <fagah@users.noreply.github.com>
2025-01-27 22:14:54 -0500

    Initial setup with basic structure

--- user-service/src/main/java/ci/mobidoc/user/UserServiceApplication.java
+++ user-service/src/main/java/ci/mobidoc/user/UserServiceApplication.java
@@ -1,16 +1,9 @@
 package ci.mobidoc.user;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(
-    title = "MobiDoc User Service API",
-    version = "1.0",
-    description = "API de gestion des utilisateurs pour MobiDoc"
-))
 public class UserServiceApplication {
 
     public static void main(String[] args) {
